293432e3fdbc1f9a8db8fc3fc088c93165dca56b
dCOMMICS <sanwurld@gmail.com>
2023-11-21 18:17:44 +0300

    💪🏿🚀

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -1,2 +1,9 @@
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+      }
+      
