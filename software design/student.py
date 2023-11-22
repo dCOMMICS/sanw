803f5770f99d63afbe2ca3ff1dcf01ef6eaaab1c
dCOMMICS <sanwurld@gmail.com>
2023-11-22 20:50:11 +0300

    💪🏿🚀

---
+++ software design/student.py
@@ -0,0 +1,34 @@
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+import opencage
+from opencage.geocoder import OpenCageGeocode
+import folium
+
+
+key = "0b561c5345d04fa39495dcdf337588c0" #Geocoder API Key need to paste here "your key" 
+number = input(" +84382485326 ")
+new_number = phonenumbers.parse(number)
+location = geocoder.description_for_number(new_number, "en")
+print(location)
+
+service_name = carrier.name_for_number(new_number,"en")
+print(service_name)
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+result = geocoder.geocode(query)
+#print(result)
+
+lat = result[0]['geometry']['lat']
+lng = result[0]['geometry']['lng']
+
+print(lat,lng)
+
+my_map = folium.Map(location=[lat,lng], zoom_start=9)
+folium.Marker([lat, lng], popup= location).add_to(my_map)
+
+my_map.save("location.html")
+
+print("location tracking completed")
+print("Thank you")
