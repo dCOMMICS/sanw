5b0b6f45e63bd5e09e9be94b5d3cc0b9f8f4ef57
dCOMMICS <sanwurld@gmail.com>
2023-11-29 23:47:15 +0300

    fibonacci (0)1

---
+++ twittercon/css/fib.java
@@ -0,0 +1,9 @@
+public int Fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    int[] fib = new int[n + 1];
+    fib[1] = 1;
+    for (int i = 2; i <= n; i++)
+        fib[i] = fib[i - 1] + fib[i - 2];
+    return fib[n];
+}
