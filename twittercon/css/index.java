7462890a151056b15aaab90c797cb0b73fd6590d
dCOMMICS <sanwurld@gmail.com>
2023-11-29 23:51:18 +0300

    🌹🚀

--- twittercon/css/index.java
+++ twittercon/css/index.java
@@ -16,3 +16,27 @@ public class Example {
 	}
 
 }
+
+
+// dsa 
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        //Kadane's algorithm
+        if(prices.length == 0) {
+            return 0;
+        }
+        
+        int min = prices[0];
+        int max = 0;
+        
+        for(int i = 1; i < prices.length; i++) {
+            if(prices[i] > min) {
+                max = Math.max(max, prices[i] - min);
+            } else {
+                min = prices[i];
+            }
+        }
+        
+        return max;
+    }
+}
