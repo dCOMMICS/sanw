5f4512c45e0fa5aad40cb6aed709d21481938b2f
dCOMMICS <sanwurld@gmail.com>
2023-11-29 23:55:49 +0300

    🔥🤮

--- twittercon/css/index.java
+++ twittercon/css/index.java
@@ -66,4 +66,21 @@ class FindAllNumbersDisappearedInAnArray {
         }
         
 
+        public class InsertInterval {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        int i = 0;
+
+        while(i < intervals.size() && intervals.get(i).end < newInterval.start) {
+            i++;
+        }
+
+        while(i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval = new Interval(Math.min(intervals.get(i).start, newInterval.start), Math.max(intervals.get(i).end, newInterval.end));
+            intervals.remove(i);
+        }
         
+        intervals.add(i, newInterval);
+
+        return intervals;
+    }
+}
