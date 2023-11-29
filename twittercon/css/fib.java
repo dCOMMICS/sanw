330c8f341cfae1fd8ad6a11132c1ad2dc02b19c5
dCOMMICS <sanwurld@gmail.com>
2023-11-29 23:47:44 +0300

    🌹🚀

--- twittercon/css/fib.java
+++ twittercon/css/fib.java
@@ -7,3 +7,16 @@ public int Fibonacci(int n) {
         fib[i] = fib[i - 1] + fib[i - 2];
     return fib[n];
 }
+
+public int Fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    int pre2 = 0, pre1 = 1;
+    int fib = 0;
+    for (int i = 2; i <= n; i++) {
+        fib = pre2 + pre1;
+        pre2 = pre1;
+        pre1 = fib;
+    }
+    return fib;
+}
