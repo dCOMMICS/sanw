e0d01bcfd555d7bd9f248e49a5d09eabb0004ae7
dCOMMICS <sanwurld@gmail.com>
2023-11-30 17:31:57 +0300

    fatal error

---
+++ twittercon/css/1.c
@@ -0,0 +1,12 @@
+#include  <studio.h>
+ int main (void)
+ {
+    int i = get_int ("what's i ?");
+    int j = get_int (" what's j?");
+ }
+
+ if(i<y)
+
+ {
+    printf (" i is less than j!\n");    
+ }
