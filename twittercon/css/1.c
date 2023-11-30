bd0f542a73bee2865a034ae25ea41196fe8c6ff1
dCOMMICS <sanwurld@gmail.com>
2023-11-30 17:39:56 +0300

    code b

--- twittercon/css/1.c
+++ twittercon/css/1.c
@@ -1,18 +1,18 @@
+
 #include <stdio.h>
 
 int main(void) {
     int i, j;
 
-    
     printf("Enter the value for i: ");
     scanf("%d", &i);
 
     printf("Enter the value for j: ");
     scanf("%d", &j);
 
-   
     if (i < j) {
         printf("i is less than j!\n");
     }
 
     return 0;
+}
