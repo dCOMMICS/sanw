2ee81760a578f873758644a48c2aafbe91f87ec8
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-08 23:52:06 +0300

    ❤

--- JS/pk.js
+++ JS/pk.js
@@ -4116,3 +4116,31 @@ restart button
 */
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+
+
+
+function handleCellClick(clickedCellEvent) {
+    /*
+    We will save the clicked html element in a variable for easier further use
+    */    
+        const clickedCell = clickedCellEvent.target;
+    /*
+    Here we will grab the 'data-cell-index' attribute from the clicked cell to identify where that cell is in our grid. 
+    Please note that the getAttribute will return a string value. Since we need an actual number we will parse it to an 
+    integer(number)
+    */
+        const clickedCellIndex = parseInt(
+          clickedCell.getAttribute('data-cell-index')
+        );
+    /* 
+    Next up we need to check whether the call has already been played, 
+    or if the game is paused. If either of those is true we will simply ignore the click.
+    */
+        if (gameState[clickedCellIndex] !== "" || !gameActive) {
+            return;
+        }
+    /* 
+    If everything if in order we will proceed with the game flow
+    */    
+        handleCellPlayed(clickedCell, clickedCellIndex);
+        handleResultValidation();
