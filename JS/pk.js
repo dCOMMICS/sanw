38b93261412b4436e1ffe2a5d204bcdb1f8e5781
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-08 23:54:10 +0300

    ❤

--- JS/pk.js
+++ JS/pk.js
@@ -4233,3 +4233,8 @@ function handleCellClick(clickedCellEvent) {
             */
                 handlePlayerChange();
             }
+
+            function handlePlayerChange() {
+                currentPlayer = currentPlayer === "X" ? "O" : "X";
+                statusDisplay.innerHTML = currentPlayerTurn();
+            }
