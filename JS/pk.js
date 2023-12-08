73b168c03179534c45820e3e08e3dff39ba9a66f
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-08 23:53:42 +0300

    ❤

--- JS/pk.js
+++ JS/pk.js
@@ -4186,3 +4186,50 @@ function handleCellClick(clickedCellEvent) {
                     return;
                 }
             }
+
+            const winningConditions = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+            function handleResultValidation() {
+                let roundWon = false;
+                for (let i = 0; i <= 7; i++) {
+                    const winCondition = winningConditions[i];
+                    let a = gameState[winCondition[0]];
+                    let b = gameState[winCondition[1]];
+                    let c = gameState[winCondition[2]];
+                    if (a === '' || b === '' || c === '') {
+                        continue;
+                    }
+                    if (a === b && b === c) {
+                        roundWon = true;
+                        break
+                    }
+                }
+            if (roundWon) {
+                    statusDisplay.innerHTML = winningMessage();
+                    gameActive = false;
+                    return;
+                }
+            /* 
+            We will check weather there are any values in our game state array 
+            that are still not populated with a player sign
+            */
+                let roundDraw = !gameState.includes("");
+                if (roundDraw) {
+                    statusDisplay.innerHTML = drawMessage();
+                    gameActive = false;
+                    return;
+                }
+            /*
+            If we get to here we know that the no one won the game yet, 
+            and that there are still moves to be played, so we continue by changing the current player.
+            */
+                handlePlayerChange();
+            }
