e1435b0c1a0535aee61a8d303fd31d986a0e945b
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-08 23:58:16 +0300

    ❤

--- JS/game.js
+++ JS/game.js
@@ -268,3 +268,112 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+import React, {Component} from "react";
+
+class TodoItems extends Component {
+	constructor(props){
+		super(props);
+		this.createTasks = this.createTasks.bind(this);
+	}
+	createTasks(item){
+		return <li onClick={() => this.delete(item.key)} key={item.key}>{item.text}</li>
+	}
+	delete(key){
+		this.props.delete(key);
+	}
+	render(){
+		var todoEntries = this.props.entries;
+		var listItems = todoEntries.map(this.createTasks);
+
+		return(
+			<ul className="theList">
+				{listItems}
+			</ul>
+		);
+	}
+};
+
+export default TodoItems;
+
+import React, {Component} from "react";
+import TodoItems from "./TodoItems";
+
+class TodoList extends Component {
+	constructor(props){
+		super(props);
+		this.state = {items: []};
+		this.addItem = this.addItem.bind(this);
+		this.deleteItem = this.deleteItem.bind(this);
+	}
+	addItem(e){
+		if(this._inputElement.value !== ""){
+			var newItem = {
+				text: this._inputElement.value, 
+				key: Date.now()
+			};
+	
+			this.setState((prevState) => {
+				return {
+					items: prevState.items.concat(newItem)
+				};
+			});
+		
+			this._inputElement.value = "";
+		}
+		console.log(this.state.items);
+		e.preventDefault();
+	}
+	deleteItem(key){
+		var filteredItems = this.state.items.filter(function(item) {
+			return (item.key !== key);
+		});
+		
+		this.setState({
+			items: filteredItems
+		});
+	}
+	render(){
+		return(
+			<div className="todoListMain">
+			  <div className="header">
+			    <form onSubmit={this.addItem}>
+			      <input ref={(a) => this._inputElement = a} placeholder="enter task">
+			      </input>
+			      <button type="submit">add</button>
+			    </form>
+			    <TodoItems entries={this.state.items} delete={this.deleteItem} />
+			  </div>
+		        </div>
+		);
+	}
+}
+
+export default TodoList; 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import TodoList from "./TodoList";
+
+
+var destination = document.querySelector('#container');
+
+
+ReactDOM.render(
+  <div>
+	<TodoList/>
+  </div>, 
+  destination
+);
