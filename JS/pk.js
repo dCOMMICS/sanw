449464124fa51a558d75c936a8b2219be1a9e37e
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-08 23:53:02 +0300

    ❤

--- JS/pk.js
+++ JS/pk.js
@@ -4154,3 +4154,35 @@ function handleCellClick(clickedCellEvent) {
                 gameState[clickedCellIndex] = currentPlayer;
                 clickedCell.innerHTML = currentPlayer;
             }
+
+            const winningConditions = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+            function handleResultValidation() {
+                let roundWon = false;
+                for (let i = 0; i <= 7; i++) {
+                    const winCondition = winningConditions[i];
+                    let a = gameState[winCondition[0]];
+                    let b = gameState[winCondition[1]];
+                    let c = gameState[winCondition[2]];
+                    if (a === '' || b === '' || c === '') {
+                        continue;
+                    }
+                    if (a === b && b === c) {
+                        roundWon = true;
+                        break
+                    }
+                }
+            if (roundWon) {
+                    statusDisplay.innerHTML = winningMessage();
+                    gameActive = false;
+                    return;
+                }
+            }
