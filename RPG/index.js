2c58c1f7e501ecd05a35b115b28ec9dda8994da4
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-09 17:42:19 +0300

    ❤

--- RPG/index.js
+++ RPG/index.js
@@ -12,13 +12,24 @@ init (){
     image.onload = () => {
         this.ctx.drawImage(image,0,0)
     };
-    image.src = "";
+    image.src = "/images/maps/DemoLower.png";
 
-
+const x = 0;
+const y = 0;
 const hero = new Image ();
 hero.onload = () => {
     // draw the hero
-    this.ctx.drawImage(hero, 0,0)
+    this.ctx.drawImage(
+        hero, 
+        0, // left cut
+        0, // top cut
+        32, // width of cut
+        32, // height of cut
+        x * 16,
+        y * 16,
+        32,
+        32
+        )
 
 }
 
