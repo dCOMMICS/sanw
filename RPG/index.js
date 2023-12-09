df3fd345cd15cf8a38c63e1ef887172113ed36d1
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-12-09 17:18:03 +0300

    👏❤

---
+++ RPG/index.js
@@ -0,0 +1,13 @@
+class Overworld{
+    constructor (config){
+        this.element = config.element;
+        this.canvas = this.element.querySelector(".game-canvas");
+        this.ctx = this.canvas.getContext("2d");
+    }
+
+init (){
+    console.log("ful it")
+}
+
+
+}
