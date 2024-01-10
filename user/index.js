aec392e3acf6ef983526376e44e9ee2c839fbd08
dCOMMICS <sanwurld@gmail.com>
2024-01-10 20:38:13 +0300

    initial commit

--- user/index.js
+++ user/index.js
@@ -21,3 +21,28 @@ $button.addEventListener('click', async () => {
     link.click()
   })
 })
+
+
+const $wqtbutton = document.querySelector('wqt-button')
+
+$button.addEventListener('click', async () => {
+  const media = await navigator.mediaDevices.getDisplayMedia({
+    video: { frameRate: { ideal: 30 } }
+  })
+  const mediarecorder = new MediaRecorder(media, {
+    mimeType: 'video/webm;codecs=vp8,opus'
+  })
+  mediarecorder.start()
+
+  const [video] = media.getVideoTracks()
+  video.addEventListener("ended", () => {
+    mediarecorder.stop()
+  })
+
+  mediarecorder.addEventListener("dataavailable", (e) => {
+    const link = document.createElement("a")
+    link.href = URL.createObjectURL(e.data)
+    link.download = "captura.webm"
+    link.click()
+  })
+})
